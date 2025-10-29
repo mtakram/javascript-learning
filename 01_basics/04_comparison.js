e223aa12dd1b6b5efe7c3340d7b1148833482d06
Tariq A <69215005+mtakram@users.noreply.github.com>
2025-10-29 12:16:55 +0500

    strict check

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+console.log(2 > 1)
+console.log(2 < 1)
+console.log(2 == 1)
+
+
+console.log("2" > 3)
+console.log(null > 0)
+
+// Here comes a strict check where we checks value and its type
+
+console.log(2 === 1)
+
+// Here we are checking are both values are of same type and then its value
+// Always try to check with strict operate to get unexpected results
+
