caa05f0bb6b2d4b1749273a621624686b09ce803
Tariq A <69215005+mtakram@users.noreply.github.com>
2025-10-29 14:10:38 +0500

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+/**
+ * Template Literals
+ * 
+ */
+
+let greeting = "Hello"
+let name = " Tariq"
+
+console.log(greeting + "," + name)
+
+console.log(`${greeting} ${name}, Welcome to my channel.`);
+
+console.log(name.includes('Tariq'));
+
+let fullName = "Tariq Akram"
+
+console.log(fullName.replace(' ', '-'))
