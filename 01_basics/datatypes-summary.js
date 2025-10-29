18c5428969ab801bd57393f071f1d6500ac8988b
Tariq A <69215005+mtakram@users.noreply.github.com>
2025-10-29 12:41:24 +0500

    primitive and non primitive data types

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,23 @@
+/**
+ * Primitive datatypes
+ * 
+ * There are 7 primitive datatypes
+ * string, number, bigint, undefined, null, boolean, symbol
+ * 
+ * stored directly in memory (stack)
+ * Immutable -- Once created, they cannot be changed
+ * Always copy the value
+ * Always compare by value
+ */
+
+
+/**
+ * Non-primitive (Reference)
+ * Other than primitive datatypes all are non-primmitive datatypes
+ * object, array, function, date, regExp, map, set
+ * 
+ * stored by reference in memory (heap)
+ * Mutable -- contents can change
+ * Always copy the reference
+ * Compared by reference
+ */
