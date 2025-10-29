52f8b0434854a9a4cdf5b31cf2f0d32f351377da
Tariq A <69215005+mtakram@users.noreply.github.com>
2025-10-29 11:42:16 +0500

    conversion of datatypes

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,24 @@
+let value = 33
+
+let stringValue = String(value)
+
+console.log(stringValue)
+console.log(typeof stringValue)
+
+
+let anotherValue = "55"
+let numberAnothervalue = Number(anotherValue)
+
+console.log(numberAnothervalue)
+console.log(typeof numberAnothervalue);
+
+let thirdValue = 0
+let booleanThirdValue = Boolean(thirdValue)
+
+console.log(booleanThirdValue)
+console.log(typeof booleanThirdValue);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
