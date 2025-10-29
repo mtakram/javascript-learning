d1ada04f995d4cf690aeede6c2e006ce59b6a58a
Tariq A <69215005+mtakram@users.noreply.github.com>
2025-10-29 11:15:38 +0500

    in this we learned about use strict, data types and typeof

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let name = "Tariq"
+let age = 30
+let isLoggedIn = false
+
+
+// number typeof number
+// bigint typeof number
+// string typeof string
+// boolean typeof boolean
+// null typeof object
+// undefined typeof undefined
+// symbol typeof
+
+// object typeof
+
